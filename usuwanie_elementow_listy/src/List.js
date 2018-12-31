f5d9ecf086e849e08775c45d2bc2076775a2bd68
realmadryt123 <jd35495@gmail.com>
2018-12-31 16:10:29 +0100

    Usuwanie osoby z listy - prosta aplikacja w react.js

---
+++ usuwanie_elementow_listy/src/List.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+let counter = 0
+class List extends Component {
+  state = {
+    items: [{
+      id: counter++,
+      name: "Jan K"
+    }, {
+      id: counter++,
+      name: "Tomasz S"
+    },
+    {
+      id: counter++,
+      name: "Mateusz B"
+    }, {
+      id: counter++,
+      name: "Przemysław R"
+    }]
+  }
+  handleDelete(item) {
+    const newState = this.state.items.slice();
+    if (newState.indexOf(item) > -1) {
+      newState.splice(newState.indexOf(item), 1);
+      this.setState({ items: newState })
+    }
+  }
+  render() {
+    const listItem = this.state.items.map((item) => {
+      return <div key={item.id}>
+        <span>{item.name}</span> <button onClick={this.handleDelete.bind(this, item)}>Usuń członka</button>
+      </div>
+    })
+    return (
+      <div>
+        <ul>
+          <li>
+            {listItem}
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default List;
