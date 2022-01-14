91c9b8613bae2fa5684024fbc6bdd3d067f91753
KonradZawodnik <konrad.zawodnik@inetum.com>
2022-01-14 21:02:45 +0100

    recruitment task for gridaly

---
+++ gridaly_recruitment_task/src/Square/Square.tsx
@@ -0,0 +1,15 @@
+import SquareBlock from "./SquareBlock/SquareBlock";
+
+import './Square.css';
+
+const App = () => {
+  return (
+    <>
+      <header className="header"></header>
+      <SquareBlock/>
+      <footer className="footer"></footer>
+    </>
+  );
+}
+
+export default App;
---
+++ gridaly_recruitment_task/src/Square/SquareBlock/SquareBlock.tsx
@@ -0,0 +1,34 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import './SquareBlock.css';
+
+const SquareBlock = () => {
+    const [clicked, setClicked] = useState<boolean>(false);
+    const [count, setCount] = useState<number>(0);
+
+    const changeColor = useCallback(() => {
+        setClicked(true);
+        setCount(count + 1);
+    }, [count]);
+
+    useEffect(() => {
+        const timer = window.setInterval(() => {
+            setClicked(false);
+            setCount(0);
+        }, 2000);
+        return () => {
+            window.clearInterval(timer);
+        }
+    }, []);
+
+    return (
+        <div
+            className={clicked ? "squareBlockAfterFirstClick" : "squareBlock"}
+            onClick={changeColor}
+            style={{ backgroundColor: count === 3 ? "#ff0000" : "" }}
+        />
+    )
+
+}
+
+export default SquareBlock;
