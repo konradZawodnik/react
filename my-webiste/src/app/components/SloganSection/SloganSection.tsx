3ed67403ae7652e763929d287d8826997fcebf97
Konrad Zawodnik <konrad.zawodnik@wp.pl>
2023-06-22 13:23:01 +0200

    update styling of header image. update text

--- my-webiste/src/app/components/SloganSection/SloganSection.tsx
+++ my-webiste/src/app/components/SloganSection/SloganSection.tsx
@@ -7,7 +7,7 @@ export const SloganSection = () => (
       <h1> O mnie</h1>
       <p className="text">
         Obecnie jestem Front-End Developerem w Sii. Pracowałem w projekcie z
-        branży telekomunikacyjnej, a obecnie pracuje w projekcie z branży
+        branży telekomunikacyjnej, a obecnie pracuję w projekcie z branży
         automotive. Stack technologiczny w obu projekatach to React, Redux,
         Material UI, Redux Toolkit, SCSS, TypeScript i React Testing Library.
         Wcześniej byłem Front-End Developerem w Inetum. Pracowałem tam w dwóch
@@ -16,14 +16,14 @@ export const SloganSection = () => (
         natomiast miał stack technologiczny: HTML, SCSS, TypeScript, Kendo-UI,
         D3, Chai, Sinon, MVC. Byłem też Front-End Developerem w ItMagination.
         Tworzyłem tam komponenty na stronie firmy z branży elektronicznej w
-        React, Redux, JavaScript, SCSS (polskie strony) oraz JQuery, JavaScript,
+        React, Redux, JavaScript, SCSS (polskie strony) oraz jQuery, JavaScript,
         SCSS (angielskie strony). Pracowałem też w Idea Banku jako Front-end
-        developer. W tworzeniu front-endu w tejże firmie korzystam z takich
-        technologii, jak JavaScript, React, Redux, SCSS ,Flow, Jest ,czy Enzyme.
+        developer. W tworzeniu front-endu w tejże firmie korzystałem z takich
+        technologii, jak JavaScript, React, Redux, SCSS, Flow, Jest, czy Enzyme.
         Jeszcze wcześniej byłem developerem testów w IT Run Consulting Sp. z
         o.o. Pisałem wówczas testy jednostkowe oraz testy integracyjne w
         Jasmine, Jest oraz Enzyme. Wspierałem też tworzenie warstwy front-endu.
-        Tworzę strony internetowe w oparciu o HTML,CSS, SCSS, JavaScript,
+        Tworzę strony internetowe w oparciu o HTML, CSS, SCSS, JavaScript,
         jQuery, React i Redux.
       </p>
     </div>
