344c96383a41c45e442ae7da8a49cef497425aed
Konrad Zawodnik <konrad.zawodnik@wp.pl>
2023-06-23 10:59:02 +0200

    Rewritte form example to nextJs, remove old version

---
+++ form-next/src/app/components/Email/Email.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Email.css';
+
+const Email = () => (
+    <label>
+        Podaj email:
+        <input
+            className="Input"
+            type="email"
+            name="email"
+        />
+    </label>
+)
+
+export default Email;
---
+++ form-next/src/app/components/Name/Name.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Name = () => (
+    <label>
+        Podaj imię:
+        <input
+            className="Input"
+            type="text"
+            name="name"
+        />
+    </label>
+)
+
+export default Name;
